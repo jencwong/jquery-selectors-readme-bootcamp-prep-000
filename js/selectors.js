74e7ee1abc0aba0c624b23712e5543ae4cd13c23
Jen W <jsc525@gmail.com>
2019-06-23 09:47:05 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+  
+}
+
+function lastImageSelector() {
+  $('img:last').click(lastImageSelector);
+}
+
+function ninjaBabySelector() {
+  $('#baby-ninja').click(ninjaBabySelector);
+}
+
+function divSelector() {
+  $('.pics').click(divSelector);
+}
+
+function firstListItem() {
+  $('#pic-list:first-child').click(firstListItem);
+}
